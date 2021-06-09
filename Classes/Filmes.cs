05bc553e62e2206e19f2b985c9554dfff309fbd5
NICKN1 <iranildo.junior@outlook.com>
2021-06-09 17:42:10 -0300

    Adcionado Tostring, além de correções de código

--- Classes/Filmes.cs
+++ Classes/Filmes.cs
@@ -2,12 +2,28 @@ namespace Projeto.App
 {
     public class Filmes : EntidadeBase
     {
-        private Genero Genero {get; set;}
+        //Aqui adcionei atributos dos quais acho necessário para um registro de um filme no sistema
+        private Genero genero {get; set;}
         private string Titulo {get; set;}
         private string Descricao {get; set;}
         private int Ano {get; set;}
         private string Classificacao {get; set;}
         private float Duracao {get; set;}
         private bool Excluido {get; set;}
+
+        //Métodos dos atributos dos filmes, adcionando aqueles que coloquei 
+        public Filmes(int id, Genero genero, string Titulo, string Descricao, int Ano, int classificacao, float Duracao)
+        {
+            this.id = id;
+            this.genero = genero;
+            this.Titulo = Titulo;
+            this.Descricao = Descricao;
+            this.Ano = Ano;
+            this.Classificacao = Classificacao;
+            this.Duracao = Duracao;
+            this.Excluido = false;
+        }
+
+
     }
 }
--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -1,15 +1,44 @@
+using System;
+
 namespace Projeto.App
 {
     public class Serie : EntidadeBase
     {
         //Estes são os atributos da classe Serie, no qual adicionei mais alguns de minha preferência
-        private Genero Genero {get; set;}
+        private Genero genero {get; set;}
         private string Titulo {get; set;}
         private string Descricao {get; set;}
         private int AnoInicio {get; set;}
         private int AnoEncerramento {get; set;}
-        private string Classificacao {get; set;}
+        private int Classificacao {get; set;}
         private bool Excluido {get; set;}
 
+            //Estes são os métodos, adcionando os atributos abordados acima
+        public Serie(int id, Genero genero, string Titulo, string Descricao, int AnoInicio, int AnoEncerramento, int Classificacao)
+        {
+            this.id = id;
+            this.genero = genero;
+            this.Titulo = Titulo;
+            this.Descricao = Descricao;
+            this.AnoInicio = AnoInicio;
+            this.AnoEncerramento = AnoEncerramento;
+            this.Classificacao = Classificacao;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano do Inicio: " + this.AnoInicio + Environment.NewLine;
+            retorno += "Ano do Encerramento: " + this.AnoEncerramento + Environment.NewLine;
+            retorno += "Classificação Indicativa: " + this.Classificacao + Environment.NewLine;
+            return retorno;
+        }
+        
+
+        
     }
 }
